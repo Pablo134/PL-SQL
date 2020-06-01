92774139ab1454d1830aae556a4e7ab6fc5aa937
Pablo134 <52747624+Pablo134@users.noreply.github.com>
2020-06-01 11:22:33 +0200

    Update fixer.sql

--- fixer.sql
+++ fixer.sql
@@ -1,1 +1,9 @@
-
+DROP TABLE da_aggiustare;
+CREATE TABLE da_aggiustare (cose VARCHAR2(1000), fixed VARCHAR2(1));
+																																
+CREATE OR REPLACE PACKAGE fixer AS
+                                                                
+                                                              
+                                                               
+																																
+																														
