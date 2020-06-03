f4332fcca149ce95312f5bef9054630d937906f1
Pablo134 <52747624+Pablo134@users.noreply.github.com>
2020-06-03 12:57:19 +0200

    Update fixer.sql

--- fixer.sql
+++ fixer.sql
@@ -1,9 +1,51 @@
+                          
+                                                              
+                                                               
+																																
 DROP TABLE da_aggiustare;
 CREATE TABLE da_aggiustare (cose VARCHAR2(1000), fixed VARCHAR2(1));
 																																
 CREATE OR REPLACE PACKAGE fixer AS
-                                                                
-                                                              
-                                                               
-																																
+
+PROCEDURE aggiusta;
+PROCEDURE aggiusta_questo(cosa_da_aggiustare VARCHAR2);
+								
+END fixer;                                                               
+/                                                             
+                                                               																																
 																														
+CREATE OR REPLACE PACKAGE BODY fixer AS
+
+PROCEDURE aggiusta_questo(cosa_da_aggiustare VARCHAR2) IS
+
+	PRAGMA AUTONOMOUS_TRANSACTION;
+BEGIN
+	INSERT INTO da_aggiustare(cose, fixed);
+	VALUES (cosa_da_aggiustare, 'N');
+	COMMIT;
+END fix_this;
+
+PROCEDURE aggiusta IS
+
+	CURSOR cursore_ottieni_cose IS
+	SELECT cose, ROWID
+		FROM da_aggiustare
+		WHERE fixed = 'N';
+
+BEGIN 
+	FOR v_cose_rec IN cursore_ottieni_cose LOOP
+	
+		EXECUTE IMMEDIATE v_cose_rec.cose;
+		
+		UPDATE da_aggiustare
+		SET fixed = 'Y';
+		WHERE ROWID = v_cose_rec.rowid;
+	END LOOP;
+	COMMIT;
+END aggiusta;
+
+END fixer;
+/
+	
+	
+																																			
